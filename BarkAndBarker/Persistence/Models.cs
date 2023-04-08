61f698e72351677ae88c554279a65098b58e2f31
d3vil401 <d3vil401@protonmail.com>
2023-04-08 13:40:33 +0200

    comments

--- BarkAndBarker/Persistence/Models.cs
+++ BarkAndBarker/Persistence/Models.cs
@@ -12,33 +12,37 @@ namespace BarkAndBarker.Persistence
     }
     public class ModelAccount
     {
-        public string SteamID;
-        public int State;
+        public string   SteamID;
+        public int      State;
         public DateTime LastLogin;
-        public string HWID;
+        public string   HWID;
 
         public static string QuerySelectAllAccounts = "SELECT * FROM barker.accounts;";
         public static string QuerySelectAccount = "SELECT * FROM barker.accounts WHERE SteamID = @SID";
         public static string QueryUpdateLastLogin = "UPDATE barker.accounts SET barker.accounts.LastLogin = CURRENT_TIMESTAMP;";
         public static string QueryUpdateHWID = "UPDATE barker.accounts SET barker.accounts.HWID = @HWID;";
         public static string QueryFindDuplicateHWID = "SELECT * FROM barker.accounts WHERE HWID = @HWID;";
+
+        public static string QueryCreateTable = ""; // TODO
     }
 
     public class ModelCharacter
     {
-        public int accountID; // Foreign Key
-        public string CharID;
-        public string Nickname;
-        public string Class;
-        public int Level;
+        public int      accountID; // Foreign Key, PK
+        public string   CharID; // PK
+        public string   Nickname;
+        public string   Class;
+        public int      Level;
         public DateTime CreatedAt;
-        public int Gender;
+        public int      Gender;
         public DateTime LastLogin;
-        public int KarmaScore;
+        public int      KarmaScore;
 
         public static string QuerySelectAllByUserAccount = "SELECT * FROM barker.characters WHERE barker.characters.accountID = @AID";
         public static string QuerySelectCharacterByID = "SELECT * FROM barker.characters WHERE barker.characters.CharID = @CID";
         public static string QueryCreateCharacter = "INSERT INTO barker.characters (`accountID`, `CharID`, `Nickname`, `Class`, `Level`, `Gender`) VALUES (@AID, @CID, @Nickname, @Class, @Level, @Gender);";
         public static string QueryOwnerAccountForCharacterID = "SELECT barker.accounts.* FROM barker.accounts, barker.characters WHERE barker.characters.CharID = @CID";
+
+        public static string QueryCreateTable = ""; // TODO
     }
 }
