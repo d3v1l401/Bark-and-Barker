b8e73116470093cf6a86c7e284f3918283986ac9
Julian Keberer <jkeberer@stud.hs-heilbronn.de>
2023-04-10 16:34:07 +0200

    added ranking packets + ranking protobuf file

--- BarkAndBarker/Network/PacketProcessors.cs
+++ BarkAndBarker/Network/PacketProcessors.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Protobuf.Collections;
 
 namespace BarkAndBarker.Network
 {
@@ -439,6 +440,51 @@ namespace BarkAndBarker.Network
             });
 
             var serial = new WrapperSerializer<SS2C_MERCHANT_LIST_RES>(response, PacketCommand.S2CAutoMatchRegRes);
+            return serial.Serialize();
+        }
+
+        public static object HandleRankingReq(ClientSession session, dynamic deserializer)
+        {
+            var request = ((WrapperDeserializer)deserializer).Parse<SC2S_RANKING_RANGE_REQ>();
+
+            var response = new SS2C_RANKING_RANGE_RES();
+            response.RankType = request.RankType;
+            response.StartIndex = request.StartIndex;
+            response.EndIndex = request.EndIndex;
+            response.CharacterClass = request.CharacterClass;
+
+            return response;
+        }
+
+        public static MemoryStream HandleRankingRes(ClientSession session, dynamic inputClass)
+        {
+            var response = (SS2C_RANKING_RANGE_RES)inputClass;
+
+            response.Result = (uint)MatchmakingResponseResult.SUCCESS;
+
+            response.AllRowCount = 1;
+
+            //TODO
+            var record = new SRankRecord
+            {
+                PageIndex = 0,
+                Rank = 1,
+                Score = 6969,
+                Percentage = 30,
+                AccountId = "421421412",
+                NickName = new SACCOUNT_NICKNAME()
+                {
+                    OriginalNickName = "BestOne",
+                    StreamingModeNickName = "BestOne",
+                    KarmaRating = 100,
+                },
+                CharacterClass = response.CharacterClass
+            };
+
+            response.Records.Add(record);
+
+
+            var serial = new WrapperSerializer<SS2C_RANKING_RANGE_RES>(response, PacketCommand.S2CRankingRangeRes);
             return serial.Serialize();
         }
     }
