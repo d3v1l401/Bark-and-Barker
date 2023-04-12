f917eaaa3e54ab469d25a3e25752a58ade0a8479
Julian Keberer <jkeberer@stud.hs-heilbronn.de>
2023-04-12 22:13:56 +0200

    moved db models to own files

---
+++ BarkAndBarker/Persistence/Models/ModelAccount.cs
@@ -0,0 +1,27 @@
+﻿namespace BarkAndBarker.Persistence.Models;
+
+public class ModelAccount : IModel
+{
+    public string SteamID;
+    public int State;
+    public DateTime LastLogin;
+    public string HWID;
+    public string LastIP;
+
+    public static readonly string QuerySelectAllAccounts = "SELECT * FROM barker.accounts;";
+    public static readonly string QuerySelectAccount = "SELECT * FROM barker.accounts WHERE SteamID = @SID";
+    public static readonly string QueryUpdateLastLogin = "UPDATE barker.accounts SET barker.accounts.LastLogin = CURRENT_TIMESTAMP;";
+    public static readonly string QueryUpdateHWID = "UPDATE barker.accounts SET barker.accounts.HWID = @HWID;";
+    public static readonly string QueryUpdateIP = "UPDATE barker.accounts SET barker.accounts.LastIP = @IP;";
+    public static readonly string QueryFindDuplicateHWID = "SELECT * FROM barker.accounts WHERE HWID = @HWID;";
+    public static readonly string QueryCreateAccount = "INSERT INTO barker.accounts (`SteamID`, `State`) VALUES (@SID, '1');";
+
+    public static readonly string QueryCreateTable = @"CREATE TABLE IF NOT EXISTS `accounts` (
+                                                              `SteamID` varchar(50) NOT NULL,
+                                                              `State` int NOT NULL DEFAULT '1',
+                                                              `LastLogin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                                              `HWID` varchar(120) DEFAULT NULL,
+                                                              `LastIP` varchar(60) DEFAULT NULL,
+                                                              PRIMARY KEY (`SteamID`)
+                                                            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
+}
---
+++ BarkAndBarker/Persistence/Models/ModelCharacter.cs
@@ -0,0 +1,34 @@
+﻿namespace BarkAndBarker.Persistence.Models;
+
+public class ModelCharacter : IModel
+{
+    public int accountID; // Foreign Key, PK
+    public string CharID; // PK
+    public string Nickname;
+    public string Class;
+    public int Level;
+    public DateTime CreatedAt;
+    public int Gender;
+    public DateTime LastLogin;
+    public int KarmaScore;
+
+    public static readonly string QuerySelectAllByUserAccount = "SELECT * FROM barker.characters WHERE barker.characters.accountID = @AID";
+    public static readonly string QuerySelectCharacterByID = "SELECT * FROM barker.characters WHERE barker.characters.CharID = @CID";
+    public static readonly string QueryCreateCharacter = "INSERT INTO barker.characters (`accountID`, `CharID`, `Nickname`, `Class`, `Level`, `Gender`) VALUES (@AID, @CID, @Nickname, @Class, @Level, @Gender);";
+    public static readonly string QueryOwnerAccountForCharacterID = "SELECT barker.accounts.* FROM barker.accounts, barker.characters WHERE barker.characters.CharID = @CID";
+
+    public static readonly string QueryCreateTable = @"CREATE TABLE IF NOT EXISTS `characters` (
+                                                              `accountID` varchar(50) NOT NULL,
+                                                              `CharID` varchar(45) NOT NULL,
+                                                              `Nickname` varchar(20) NOT NULL,
+                                                              `Class` varchar(60) NOT NULL,
+                                                              `Level` int NOT NULL DEFAULT '1',
+                                                              `CreatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                                              `Gender` int NOT NULL,
+                                                              `LastLogin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                                              `KarmaScore` int NOT NULL DEFAULT '0',
+                                                              `IsDeleted` timestamp NULL DEFAULT NULL,
+                                                              PRIMARY KEY (`accountID`,`CharID`),
+                                                              CONSTRAINT `charOwner` FOREIGN KEY (`accountID`) REFERENCES `accounts` (`SteamID`)
+                                                            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
+}
