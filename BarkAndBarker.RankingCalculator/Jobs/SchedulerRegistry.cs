251f1241268716781182e106d640f49abe58551d
Julian Keberer <jkeberer@stud.hs-heilbronn.de>
2023-04-13 21:54:00 +0200

    added ranking fetch job prototype

---
+++ BarkAndBarker.RankingCalculator/Jobs/SchedulerRegistry.cs
@@ -0,0 +1,13 @@
+﻿using FluentScheduler;
+
+namespace BarkAndBarker.RankingCalculator.Jobs
+{
+    internal class SchedulerRegistry : Registry
+    {
+        public SchedulerRegistry()
+        {
+            //TODO Change interval later if db grows bigger / on productive use
+            Schedule<FetchRankingJob>().NonReentrant().ToRunNow().AndEvery(1).Minutes();
+        }
+    }
+}
--- BarkAndBarker.RankingCalculator/Program.cs
+++ BarkAndBarker.RankingCalculator/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Bark!");
+﻿using BarkAndBarker.RankingCalculator.Jobs;
+using FluentScheduler;
+
+Console.WriteLine("Bark!");
+
+var jobRegistry = new SchedulerRegistry();
+
+JobManager.JobException += info => Console.WriteLine("An error just happened with a scheduled job: " + info.Exception);
+JobManager.JobStart += info => Console.WriteLine($"{info.Name}: started");
+JobManager.JobEnd += info => Console.WriteLine($"{info.Name}: ended ({info.Duration})");
+
+JobManager.Initialize(jobRegistry);
+
+
+while (true)
+{
+    Console.WriteLine("Input command:");
+    var command = Console.ReadLine().Split(' ');
+    if(command.Length == 0) continue;
+
+    if (command[0] == "exit") return;
+}
