c44af7e3f2c9f2696d0bb2c15633cab178ba72cf
Julian Keberer <jkeberer@stud.hs-heilbronn.de>
2023-04-19 20:02:50 +0200

    added split/fragmented packet support

--- BarkAndBarker.Proxy/PacketAnalyzer.cs
+++ BarkAndBarker.Proxy/PacketAnalyzer.cs
@@ -31,15 +31,32 @@ namespace BarkAndBarker.Proxy
 
             try
             {
-                var memoryStream = new MemoryStream();
-                memoryStream.Write(buffer, 0, buffer.Length);
+                var packetLengthBytes = internalBuffer.Take(4).ToArray();
+                if (BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(packetLengthBytes); // Reverse the byte order if running on a little-endian system
+                }
+                var packetLength = System.Net.IPAddress.NetworkToHostOrder(BitConverter.ToInt32(packetLengthBytes, 0));
 
-                //var deser = new WrapperDeserializer(buffer);
-                //
-                //var packetType = deser.GetPacketClass().ToString();
-                //
-                //analyzedLogger.Log(packetType);
 
+                if (internalBuffer.Count >= packetLength)
+                {
+                    var currPacketBuffer = new byte[packetLength];
+
+                    for (int i = 0; i < packetLength; i++)
+                    {
+                        currPacketBuffer[i] = internalBuffer.Dequeue();
+                    }
+
+                    var memoryStream = new MemoryStream();
+                    memoryStream.Write(currPacketBuffer, 0, currPacketBuffer.Length);
+
+                    var deser = new WrapperDeserializer(memoryStream);
+                    
+                    var packetType = deser.GetPacketClass().ToString();
+                    
+                    analyzedLogger.Log(packetType);
+                }
             }
             catch (Exception ex)
             {
