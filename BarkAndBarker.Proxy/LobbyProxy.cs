79f64c3bfa3c797f84b4301d575828479d1c792f
Julian Keberer <jkeberer@stud.hs-heilbronn.de>
2023-04-19 09:18:04 +0200

    added first extremely limited proxy prototype attemp

---
+++ BarkAndBarker.Proxy/LobbyProxy.cs
@@ -0,0 +1,76 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace BarkAndBarker.Proxy
+{
+    internal class LobbyProxy
+    {
+        private readonly string localAddress;
+        private readonly int localPort;
+        private readonly string remoteAddress;
+        private readonly int remotePort;
+
+        public LobbyProxy(string localAddress, int localPort, string remoteAddress, int remotePort)
+        {
+            this.localAddress = localAddress;
+            this.localPort = localPort;
+            this.remoteAddress = remoteAddress;
+            this.remotePort = remotePort;
+        }
+
+        public void Start()
+        {
+            var listener = new TcpListener(IPAddress.Parse(localAddress), localPort);
+            listener.Start();
+
+            Console.WriteLine($"Listening on {localAddress}:{localPort}");
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                var clientThread = new Thread(() => HandleClient(client));
+                clientThread.Start();
+            }
+        }
+
+        private void HandleClient(TcpClient client)
+        {
+            using (client)
+            {
+                try
+                {
+                    using var remoteClient = new TcpClient();
+                    remoteClient.Connect(remoteAddress, remotePort);
+
+                    var clientToRemoteThread =
+                        new Thread(() => ForwardTraffic(client.GetStream(), remoteClient.GetStream()));
+                    var remoteToClientThread =
+                        new Thread(() => ForwardTraffic(remoteClient.GetStream(), client.GetStream()));
+
+                    clientToRemoteThread.Start();
+                    remoteToClientThread.Start();
+
+                    clientToRemoteThread.Join();
+                    remoteToClientThread.Join();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error handling client: {ex.Message}");
+                }
+            }
+        }
+
+        private void ForwardTraffic(NetworkStream inputStream, NetworkStream outputStream)
+        {
+            var buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = inputStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                string packetData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Packet data: {packetData}");
+
+                outputStream.Write(buffer, 0, bytesRead);
+            }
+        }
+    }
+}
---
+++ BarkAndBarker.Proxy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace BarkAndBarker.Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var centralProxy = new CentralProxy("127.0.0.1", 30000, "54.148.133.180", 30000);
+            centralProxy.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
