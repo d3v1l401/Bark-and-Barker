3f0bc91b1773047ccae8545696f909ee5a96be48
d3vil401 <d3vil401@protonmail.com>
2023-04-19 22:55:49 +0200

    begin

---
+++ BarkAndBarker.Shared/Persistence/Models/ModelInventory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarkAndBarker.Shared.Persistence.Models
+{
+    public class ModelInventoryItem : IModel
+    {
+        public int OwnerID { get; set; }
+        public long UniqueID { get; set; }
+        public string ItemBlueprint { get; set; }
+        public List<ModelProperty> Properties { get; set; }
+
+        public static readonly string QueryCreateTable = @"CREATE TABLE IF NOT EXISTS `inventory_items` (
+                                                              `OwnerID` varchar(45) NOT NULL,
+                                                              `UniqueID` INT NOT NULL DEFAULT AUTO_INCREMENT,
+                                                              `Nickname` varchar(20) NOT NULL,
+                                                              `Class` varchar(60) NOT NULL,
+                                                              `Level` int NOT NULL DEFAULT '1',
+                                                              `CreatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                                              `Gender` int NOT NULL,
+                                                              `LastLogin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                                              `KarmaScore` int NOT NULL DEFAULT '0',
+                                                              `IsDeleted` timestamp NULL DEFAULT NULL,
+                                                              PRIMARY KEY (`CharID`),
+                                                              CONSTRAINT `charOwner` FOREIGN KEY (`accountID`) REFERENCES `accounts` (`ID`)
+                                                            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
+    }
+}
---
+++ BarkAndBarker.Shared/Persistence/Models/ModelProperty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarkAndBarker.Shared.Persistence.Models
+{
+    public class ModelProperty : IModel
+    {
+        public string PropertyBlueprint { get; set; }
+        public short PropertyValue { get; set; }
+    }
+}
