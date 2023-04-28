3eac787881ea725da600afb53386090fcd8da952
Primitheus <Primitheus@gmail.com>
2023-04-28 03:41:09 +0100

    Work in progress, moving gold and gold calculation + persistence

--- BarkAndBarker.Shared/Persistence/Models/ModelInventory.cs
+++ BarkAndBarker.Shared/Persistence/Models/ModelInventory.cs
@@ -16,6 +16,7 @@ namespace BarkAndBarker.Shared.Persistence.Models
         public int InventoryID { get; set; }
         public int SlotID { get; set; }
         public int ItemContentsCount { get; set; }
+        public int ItemAmmoCount { get; set; }
         public List<ModelProperty> Properties { get; set; }
 
 
@@ -29,10 +30,21 @@ namespace BarkAndBarker.Shared.Persistence.Models
                                                               `InventoryID` int NOT NULL,
                                                               `SlotID` int NOT NULL,
                                                               `ItemContentsCount` int,
+                                                              `ItemAmmoCount` int,
                                                               PRIMARY KEY (`UniqueID`),
                                                               KEY `itemOwner_idx` (`OwnerID`),
                                                               CONSTRAINT `itemOwner` FOREIGN KEY (`OwnerID`) REFERENCES `characters` (`CharID`) ON DELETE CASCADE
                                                             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
+
+        public static readonly string QueryUpdateInventory = "UPDATE `barker`.`inventory_items` SET `SlotID` = @SlotID," +
+                                                              "`ItemBlueprint` = @ItemBlueprint, `ItemCount` = @ItemCount," +
+                                                              "`ItemContentsCount` = @ItemContentsCount, `ItemAmmoCount` = @ItemAmmoCount, " +
+                                                              "`InventoryID` = @InventoryID WHERE (`UniqueID` = @UniqueID);\r\n";
+
+        public static readonly string QueryDeleteInventoryGold = "DELETE FROM `barker`.`inventory_items` WHERE (`UniqueID` = @UniqueID);";
+        public static readonly string QueryInventoryGold = "SELECT * FROM `barker`.`inventory_items` WHERE (`UniqueID` = @UniqueID);";
+        public static readonly string QueryUpdateInventoryGold = "UPDATE `barker`.`inventory_items` SET `ItemCount` = @ItemCount, `SlotID` = @SlotID WHERE (`UniqueID` = @UniqueID);";
+        public static readonly string QueryInsertInventoryGold = "INSERT INTO `barker`.`inventory_items` (`OwnerID`, `UniqueID`, `ItemBlueprint`, `ItemCount`, `InventoryID`, `SlotID`) VALUES (@OwnerID, @UniqueID, @ItemID, @ItemCount, @InventoryID, @SlotID);";
 
         public static readonly int TableCreationOrder = 98;
     }
--- BarkAndBarker/Network/PacketProcessor/InventoryProcessors.cs
+++ BarkAndBarker/Network/PacketProcessor/InventoryProcessors.cs
@@ -1,5 +1,6 @@
 ﻿using Azure;
 using Azure.Core;
+using BarkAndBarker.Shared.Persistence.Models;
 using DC.Packet;
 using MySqlX.XDevAPI.Common;
 using System;
@@ -7,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace BarkAndBarker.Network.PacketProcessor
 {
@@ -43,12 +45,102 @@ namespace BarkAndBarker.Network.PacketProcessor
 
             Console.WriteLine(request.ToString());
 
-            response.Result = 1;
-            response.OldItem.Add(request.OldItem);
-            response.NewItem.Add(request.NewItem);
+            if (request.NewItem.Count > 1) {
+                response.Result = 0;
+                return response;
+            }
+
+            
+
+            var newItem = request.NewItem[0];
+            var oldItem = request.OldItem[0];
+
+
+            if (newItem.ItemUniqueId != oldItem.ItemUniqueId && newItem.ItemId == "DesignDataItem:Id_Item_GoldCoins" && (newItem.ItemCount - oldItem.ItemCount == 0))
+            {
+
+                var queryGoldCount = session.GetDB().Select<ModelInventoryItem>(ModelInventoryItem.QueryInventoryGold, new { UniqueID = oldItem.ItemUniqueId });
+                int goldcount = 0;
+
+                foreach (var item in queryGoldCount)
+                {
+                   goldcount = item.ItemCount;
+                }
+
+                if (goldcount - newItem.ItemCount <= 0)
+                {
+                    Console.WriteLine("Gold has depleted and will be deleting.");
+
+                    var queryResGold = session.GetDB().Execute(ModelInventoryItem.QueryDeleteInventoryGold, new
+                    {
+                        UniqueID = oldItem.ItemUniqueId,
+                        //ItemCount = -1
+
+                    });
+
+                    var queryUpdateGold = session.GetDB().Execute(ModelInventoryItem.QueryUpdateInventoryGold, new
+                    {
+                        UniqueID = oldItem.ItemUniqueId,
+                        ItemCount = newItem.ItemCount + goldcount,
+                        SlotID = newItem.SlotId
+                    }); 
+                }
+                else
+                {
+                    Console.WriteLine("Taking a gold out of stack.");
+
+                    var queryInsertGold = session.GetDB().Execute(ModelInventoryItem.QueryInsertInventoryGold, new
+                    {
+                        OwnerID = session.m_currentCharacter.CharID,
+                        UniqueID = newItem.ItemUniqueId,
+                        ItemCount = newItem.ItemCount,
+                        ItemID = newItem.ItemId,
+                        InventoryID = newItem.InventoryId,
+                        SlotID = newItem.SlotId
+
+                    });
+
+                    var queryResGold = session.GetDB().Execute(ModelInventoryItem.QueryUpdateInventoryGold, new
+                    {
+                        UniqueID = oldItem.ItemUniqueId,
+                        ItemCount = goldcount - newItem.ItemCount,
+                        SlotID = oldItem.SlotId
+
+                    });;
+
+                 
+
+                }
+
+
+                
+            }
+
+            //var GoldCount = newItem.ItemCount + oldItem.ItemCount;
+
+
+            var queryRes = session.GetDB().Execute(ModelInventoryItem.QueryUpdateInventoryGold, new
+            {
+                UniqueID = newItem.ItemUniqueId,
+                ItemCount = newItem.ItemCount,
+                SlotID = newItem.SlotId
+
+            });
+
+
+            Console.WriteLine(queryRes.ToString());
+
+
+            if (queryRes > 0)
+            {
+                response.Result = 1;
+                response.OldItem.Add(request.OldItem);
+                response.NewItem.Add(request.NewItem);
+            }
+            else
+                response.Result = 0;
 
             return response;
-
         }
 
         public static MemoryStream HandleInventorySingleUpdateRes(ClientSession session, dynamic inputClass)
